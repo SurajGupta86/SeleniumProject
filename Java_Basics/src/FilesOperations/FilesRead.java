d2ac36d5e88e54ff19f35b5a8583f6d48e4759b0
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2023-12-27 22:20:06 +0530

    Done

--- Java_Basics/src/FilesOperations/FilesRead.java
+++ Java_Basics/src/FilesOperations/FilesRead.java
@@ -10,7 +10,7 @@ public class FilesRead {
 
 
 		public static void main(String[] args) throws IOException {
-			
+			int a=1;
 //			Scanner sc = new Scanner(System.in);
 //			System.out.println("Enter the name to be filled in file: ");
 //			String name = sc.nextLine();
