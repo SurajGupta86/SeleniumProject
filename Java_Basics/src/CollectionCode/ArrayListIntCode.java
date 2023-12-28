cfb6137c660f9b90b2c616e46a41ce71cf3c23c1
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2023-12-28 21:28:45 +0530

    D

--- Java_Basics/src/CollectionCode/ArrayListIntCode.java
+++ Java_Basics/src/CollectionCode/ArrayListIntCode.java
@@ -13,10 +13,15 @@ public class ArrayListIntCode {
 		lt.add(10);
 		lt.add(20);
 		lt.add(30);
-		lt.add(null);
+		lt.add(null);  //null value can be considered as a integer value
 		
 		lt.remove(1);
 		lt.add(1, 40);
+		
+		for(Integer i:lt) {   //Print the values one by one in a line
+			System.out.println(i);
+		}
+		
 		int i = lt.get(0);  //No need to type case because the variable is int and also the ArrayList of type int
 		System.out.println(lt.subList(1, 4));  //Returns a particular range of value from the ArrayList
 		System.out.println(lt);  //Prints the entire list
