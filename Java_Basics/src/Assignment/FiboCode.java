146d47122f7f4b12cc9ce7d35f2794db42958f3f
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-08 11:14:13 +0530

    Done

---
+++ Java_Basics/src/Assignment/FiboCode.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class FiboCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to print fibo series: ");
+		int n = sc.nextInt();
+		int a=0,b=1;
+		System.out.print("The fibonacci series:");
+		for(int i=0;i<n;i++) {
+			System.out.print("\t"+a);
+			int temp=a+b;
+			a=b;
+			b=temp;
+		}
+	}
+
+}
