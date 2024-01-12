f10b48a4dd21ea7e71b530558eecbd10325ceda1
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-12 12:21:57 +0530

    DONE

--- Java_Basics/src/Constructors/TestSubClassA.java
+++ Java_Basics/src/Constructors/TestSubClassA.java
@@ -9,9 +9,9 @@ public class TestSubClassA extends TestClass{
 
 	
 	@Override
-	public void TestClass1(String s) {
-		super.TestClass1(s);               //For calling a concrete method we use 'Super' keyword ie-> super.methodname();
-		System.out.println("Hello, My name is "+s);
+	public void TestClass1(String k) {
+		super.TestClass1(k);               //For calling a concrete method we use 'Super' keyword ie-> super.methodname();
+		System.out.println("Hello, My name is "+k);
 	}
 
 	public static void main(String[] args) {
